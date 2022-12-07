e4dc62670f9cb3e7e95dff6dbd640083f75783a4
TheChampu <117635421+TheChampu@users.noreply.github.com>
2022-12-07 20:18:44 +0530

    Update sudoers.py

--- Champu/plugins/sudo/sudoers.py
+++ Champu/plugins/sudo/sudoers.py
@@ -67,7 +67,7 @@ async def useradd(client, message: Message, _):
 async def userdel(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Due to bot's privacy issues, You can't manage sudo users when you're using Yukki's Database.\n\n Please fill your MONGO_DB_URI in your vars to use this feature**"
+            "**Due to bot's privacy issues, You can't manage sudo users when you're using Champu's Database.\n\n Please fill your MONGO_DB_URI in your vars to use this feature**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
