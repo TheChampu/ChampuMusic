ae6667ca100e086e9c80c1fdaf55329cbfabe0db
Champu <117635421+TheChampu@users.noreply.github.com>
2024-11-04 21:11:28 +0530

    Update invitelink.py

--- ChampuMusic/plugins/tools/invitelink.py
+++ ChampuMusic/plugins/tools/invitelink.py
@@ -1,11 +1,30 @@
 import os
-
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
-
 from ChampuMusic import app
 from ChampuMusic.misc import SUDOERS
+from pyrogram.enums import ChatMemberStatus
+import asyncio
+
+
+
+
+@app.on_message(filters.command("leave") & SUDOERS)
+async def leave(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ɢʀᴏᴜᴘ ɪᴅ. ᴜsᴇ ʟɪᴋᴇ: /leave chat_id.")
+    try:
+        chat_id = int(message.command[1])
+    except ValueError:
+        return await message.reply_text(f"ɪɴᴠᴀʟɪᴅ ᴄʜᴀᴛ ɪᴅ. ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴀ ɴᴜᴍᴇʀɪᴄ ɪᴅ.")
+    CHAMPU = await message.reply_text(f"ʟᴇᴀᴠɪɴɢ ᴄʜᴀᴛ... {app.me.mention}")
+    try:
+        await app.send_message(chat_id, f"{app.me.mention} ʟᴇғᴛɪɴɢ ᴄʜᴀᴛ ʙʏᴇ...")
+        await app.leave_chat(chat_id)
+        await CHAMPU.edit(f"{app.me.mention} ʟᴇғᴛ ᴄʜᴀᴛ {chat_id}.")
+    except Exception as e:
+        pass
 
 
 # Command handler for /givelink command
@@ -14,7 +33,7 @@ async def give_link_command(client, message):
     # Generate an invite link for the chat where the command is used
     chat = message.chat.id
     link = await app.export_chat_invite_link(chat)
-    await message.reply_text(f"Here's the invite link for this chat:\n{link}")
+    await message.reply_text(f"ʜᴇʀᴇ's ᴛʜᴇ ɪɴᴠɪᴛᴇ ʟɪɴᴋ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ:\n{link}")
 
 
 @app.on_message(
@@ -25,7 +44,7 @@ async def give_link_command(client, message):
 )
 async def link_command_handler(client: Client, message: Message):
     if len(message.command) != 2:
-        await message.reply("Invalid usage. Correct format: /link group_id")
+        await message.reply("ɪɴᴠᴀʟɪᴅ ᴜsᴀɢᴇ. ᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛ: /link group_id")
         return
 
     group_id = message.command[1]
@@ -35,29 +54,29 @@ async def link_command_handler(client: Client, message: Message):
         chat = await client.get_chat(int(group_id))
 
         if chat is None:
-            await message.reply("Unable to get information for the specified group ID.")
+            await message.reply("ᴜɴᴀʙʟᴇ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғᴏʀ ᴛʜᴇ sᴘᴇᴄɪғɪᴇᴅ ɢʀᴏᴜᴘ ɪᴅ.")
             return
 
         try:
             invite_link = await client.export_chat_invite_link(chat.id)
         except FloodWait as e:
-            await message.reply(f"FloodWait: {e.x} seconds. Retrying in {e.x} seconds.")
+            await message.reply(f"ғʟᴏᴏᴅᴡᴀɪᴛ: {e.x} sᴇᴄᴏɴᴅs. ʀᴇᴛʀʏɪɴɢ ɪɴ {e.x} sᴇᴄᴏɴᴅs.")
             return
 
         group_data = {
-            "id": chat.id,
-            "type": str(chat.type),
-            "title": chat.title,
-            "members_count": chat.members_count,
-            "description": chat.description,
-            "invite_link": invite_link,
-            "is_verified": chat.is_verified,
-            "is_restricted": chat.is_restricted,
-            "is_creator": chat.is_creator,
-            "is_scam": chat.is_scam,
-            "is_fake": chat.is_fake,
-            "dc_id": chat.dc_id,
-            "has_protected_content": chat.has_protected_content,
+            "ɪᴅ": chat.id,
+            "ᴛʏᴘᴇ": str(chat.type),
+            "ᴛɪᴛʟᴇ": chat.title,
+            "ᴍᴇᴍʙᴇʀs_ᴄᴏᴜɴᴛ": chat.members_count,
+            "ᴅᴇsᴄʀɪᴘᴛɪᴏɴ": chat.description,
+            "ɪɴᴠɪᴛᴇ_ʟɪɴᴋ": invite_link,
+            "ɪs_ᴠᴇʀɪғɪᴇᴅ": chat.is_verified,
+            "ɪs_ʀᴇsᴛʀɪᴄᴛᴇᴅ": chat.is_restricted,
+            "ɪs_ᴄʀᴇᴀᴛᴏʀ": chat.is_creator,
+            "ɪs_sᴄᴀᴍ": chat.is_scam,
+            "ɪs_ғᴀᴋᴇ": chat.is_fake,
+            "ᴅᴄ_ɪᴅ": chat.dc_id,
+            "ʜᴀs_ᴘʀᴏᴛᴇᴄᴛᴇᴅ_ᴄᴏɴᴛᴇɴᴛ": chat.has_protected_content,
         }
 
         with open(file_name, "w", encoding="utf-8") as file:
@@ -67,7 +86,7 @@ async def link_command_handler(client: Client, message: Message):
         await client.send_document(
             chat_id=message.chat.id,
             document=file_name,
-            caption=f"𝘏𝘦𝘳𝘦 𝘐𝘴 𝘵𝘩𝘦 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘍𝘰𝘳\n{chat.title}\n𝘛𝘩𝘦 𝘎𝘳𝘰𝘶𝘱 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘚𝘤𝘳𝘢𝘱𝘦𝘥 𝘉𝘺 : @{app.username}",
+            caption=f"ʜᴇʀᴇ ɪs ᴛʜᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғᴏʀ\n{chat.title}\nᴛʜᴇ ɢʀᴏᴜᴘ ɪɴғᴏʀᴍᴀᴛɪᴏɴ sᴄʀᴀᴘᴇᴅ ʙʏ : @{app.username}",
         )
 
     except Exception as e:
